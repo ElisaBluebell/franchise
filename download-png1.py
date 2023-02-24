f10f3b2353f5f78fae4158e6cb2ad7c77d7e6cd6
ElisaBluebell <selfishracer@hotmail.com>
2023-02-24 09:59:23 +0900

    230224 practice

--- download-png1.py
+++ download-png1.py
@@ -1,6 +1,10 @@
+# 라이브러리 읽어 들이기 --- (※1)
 import urllib.request
+
+# URL과 저장 경로 지정하기
 url = "http://dalong.net/reviews/pg/p02/p/p02_01.jpg"
 savename = "test.png"
 
+# 다운로드 --- (※2)
 urllib.request.urlretrieve(url, savename)
 print("저장되었습니다...!")
