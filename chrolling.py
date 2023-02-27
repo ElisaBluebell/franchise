f08b0a33a3c48be0d437cae9a50042d76b53ba0a
ElisaBluebell <selfishracer@hotmail.com>
2023-02-27 11:52:05 +0900

    230227 practice Elisa Bluebell

--- chrolling.py
+++ chrolling.py
@@ -17,31 +17,33 @@ driver.implicitly_wait(300)
 # search_box1.submit()
 
 driver.get('https://map.kakao.com/')
-search_line = driver.find_element(By.XPATH, """//*[@id="search.keyword.query"]""")
-search_line.send_keys("제주도 카페")
-search_line.send_keys(Keys.ENTER)
 
+search_line = driver.find_element(By.XPATH, """//*[@id="search.keyword.query"]""")
+search_line.send_keys("제주도 스타벅스")
+search_line.send_keys(Keys.ENTER)
 time.sleep(1)
 
 driver.find_element(By.XPATH, """//*[@id="info.search.place.more"]""").send_keys(Keys.ENTER)
+time.sleep(1)
+
 page = driver.find_element(By.XPATH, """//*[@id="info.search.place.cnt"]""").text
 page = page.replace(",", "")
 temp_page = int(page)
 page = (temp_page // 15) + 1
 
-f = open("jeju_cafe_list.csv", "w", encoding="utf-8")
+f = open("jeju_starbucks_list.csv", "w", encoding="utf-8")
 f.write("업소명, 주소, 리뷰")
 
-for j in range(1, page + 1):
+for j in range(1, page):
     for i in range(1, 16):
         temp_address = driver.find_element(By.XPATH, f"""//*[@id="info.search.place.list"]/li[{i}]/div[5]/div[2]/p[1]""").text
         temp_name = driver.find_element(By.XPATH, f"""//*[@id="info.search.place.list"]/li[{i}]/div[3]/strong/a[2]""").text
         temp_review = driver.find_element(By.XPATH, f"""//*[@id="info.search.place.list"]/li[{i}]/div[4]/a/em""").text
         if temp_address[:6] == "제주특별자치":
-            print("카페명: ", temp_name, "카페 주소: ", temp_address, "리뷰 갯수: ", temp_review)
+            print("업소명: ", temp_name, "주소: ", temp_address, "리뷰: ", temp_review)
             f.write(temp_name + "," + temp_address + "," + temp_review + "\n")
-            time.sleep(0.5)
-
+            time.sleep(1)
+#
     if page % 5 == 0:
         driver.find_element(By.XPATH, """// *[ @ id = "info.search.page.next"]""").send_keys(Keys.ENTER)
         time.sleep(1)
@@ -63,4 +65,4 @@ f.close()
 # print("h1 =", h1)
 # url = ""
 
-a = input()
+# a = input()
