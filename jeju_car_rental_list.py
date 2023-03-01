3234f078f8543d3fe85638575115830a46c0e519
ElisaBluebell <selfishracer@hotmail.com>
2023-03-01 18:13:45 +0900

    230228 practice Elisa Bluebell

--- jeju_car_rental_list.py
+++ jeju_car_rental_list.py
@@ -1,100 +1,7 @@
 import collections
 import requests
-import folium
 import pandas as pd
 import numpy as np
-from folium.plugins import MiniMap
-#
-#
-# # 카카오 API
-# def whole_region(keyword, start_x, start_y, end_x, end_y):
-#     page_num = 1
-#     data_list = []
-#
-#     while True:
-#         url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
-#         params = {'query': keyword, 'page': page_num, 'rect': f'''''{start_x},{start_y},{end_x},{end_y}'''}
-#         headers = {'Authorization': 'KakaoAK 3e6f33dd880519c5be39333ce69e712a'}
-#         resp = requests.get(url, params=params, headers=headers)
-#
-#         search_count = resp.json()['meta']['total_count']
-#         print('총 개수', search_count)
-#
-#         if search_count > 45:
-#             print('좌표 4등분')
-#             dividing_x = (start_x + end_x) / 2
-#             dividing_y = (start_y + end_y) / 2
-#
-#             data_list.extend(whole_region(keyword, start_x, start_y, dividing_x, dividing_y))
-#             data_list.extend(whole_region(keyword, dividing_x, start_y, end_x, dividing_y))
-#             data_list.extend(whole_region(keyword, start_x, dividing_y, dividing_x, end_y))
-#             data_list.extend(whole_region(keyword, dividing_x, dividing_y, end_x, end_y))
-#             return data_list
-#
-#         else:
-#             if resp.json()['meta']['is_end']:
-#                 data_list.extend(resp.json()['documents'])
-#                 return data_list
-#
-#             else:
-#                 print('다음페이지')
-#                 page_num += 1
-#                 data_list.extend(resp.json()['documents'])
-#
-#
-# def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y):
-#     # 최종 데이터가 담길 리스트
-#     overlapped_result = []
-#
-#     # 지도를 사각형으로 나누면서 받아옴
-#     for i in range(1, num_x + 1):
-#         end_x = start_x + next_x
-#         initial_start_y = start_y
-#         for j in range(1, num_y + 1):
-#             end_y = initial_start_y + next_y
-#             each_result = whole_region(keyword, start_x, initial_start_y, end_x, end_y)
-#             overlapped_result.extend(each_result)
-#             initial_start_y = end_y
-#         start_x = end_x
-#
-#     return overlapped_result
-#
-#
-# keyword = '카페'
-# start_x = 126.14
-# start_y = 33.11
-# next_x = 0.01
-# next_y = 0.01
-# num_x = 92
-# num_y = 58
-#
-# overlapped_result = overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y)
-#
-# results = list(map(dict, collections.OrderedDict.fromkeys(tuple(sorted(d.teims())) for d in overlapped_result)))
-# X = []
-# Y = []
-# stores = []
-# road_address = []
-# place_url = []
-# ID = []
-# for place in results:
-#
-#     X.append(float(place['x']))
-#     Y.append(float(place['y']))
-#     stores.append(place['place_name'])
-#     road_address.append(place['road_address_name'])
-#     place_url.append(place['place_url'])
-#     ID.append(place['id'])
-#
-#     ar = np.array([ID, stores, X, Y, road_address, place_url]).T
-#     df = pd.DataFrame(ar, columns = ['ID', 'stores', 'X', 'Y', 'road_address', 'place_url'])
-#
-#     print('total_result_number = ', len(df))
-#     print(results)
-#
-#
-#
-#
 
 ##카카오 API
 def whole_region(keyword, start_x, start_y, end_x, end_y):
@@ -157,7 +64,7 @@ def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y):
     return overlapped_result
 
 # 시작 x 좌표 및 증가값
-keyword = '대형마트'
+keyword = '렌트카'
 start_x = 126.14
 start_y = 33.11
 next_x = 0.01
@@ -188,8 +95,8 @@ for place in results:
 
 print(results)
 
-# 대형마트 기본 정보 저장
-file_name = "jeju_mart_list"
+# 렌트카 기본 정보 저장
+file_name = "jeju_car_rental_list"
 f = open(f"{file_name}.csv", "w", encoding="utf-8")
 f.write("id, store, business, location, x, y, url\n")
 for i in range(len(results)):
--- jeju_catholic_church_list.py
+++ jeju_catholic_church_list.py
@@ -157,7 +157,7 @@ def overlapped_data(keyword, start_x, start_y, next_x, next_y, num_x, num_y):
     return overlapped_result
 
 # 시작 x 좌표 및 증가값
-keyword = '대형마트'
+keyword = '성당'
 start_x = 126.14
 start_y = 33.11
 next_x = 0.01
@@ -188,8 +188,8 @@ for place in results:
 
 print(results)
 
-# 대형마트 기본 정보 저장
-file_name = "jeju_mart_list"
+# 성당 기본 정보 저장
+file_name = "jeju_catholic_church_list"
 f = open(f"{file_name}.csv", "w", encoding="utf-8")
 f.write("id, store, business, location, x, y, url\n")
 for i in range(len(results)):
